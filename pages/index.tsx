b90125ee6c94a8499858191053d27c667a10c0a5
dwestland <don@westland.net>
2023-03-06 21:29:21 -0800

    Update UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,73 @@
 import React from 'react'
-
+import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
+import contentData from '@/common/content-data'
 
-const TwoColumnPage = () => (
-  <Layout title="Document" description="Document description">
-    <main className="two-column">
-      <section>
-        <h1>Westland Post</h1>
-        <h3>How AI is impacting tech, the arts and mankind</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-          obcaecati quam commodi perspiciatis aut sequi, corporis numquam
-          reiciendis necessitatibus eligendi deserunt voluptatem iste ea
-          delectus magnam. Excepturi, esse suscipit facere dolore deleniti
-          temporibus odit inventore, beatae possimus quae placeat quasi
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-          obcaecati quam commodi perspiciatis aut sequi, corporis numquam
-          reiciendis necessitatibus eligendi deserunt voluptatem iste ea
-          delectus magnam. Excepturi, esse suscipit facere dolore deleniti
-          temporibus odit inventore, beatae possimus quae placeat quasi
-        </p>
-      </section>
-      <aside>
-        <h3>Image Gallery</h3>
-        <Image
-          src="/images/grace-hopper.jpg"
-          width={600}
-          height={400}
-          alt="Grace Hopper"
-        />
-        <br />
-        <br />
-        <Image src="/images/eniac.jpg" width={600} height={400} alt="ENIAC" />
-        <br />
-        <br />
-        <Image src="/images/ibm.jpg" width={600} height={400} alt="IBM" />
-      </aside>
-    </main>
-  </Layout>
-)
+const TwoColumnPage = () => {
+  const sendinblueApiKey = 'dtjvq2N0k79QpsRC'
+  console.log('%c boom ', 'background: red; color: white')
 
+  const articles = contentData.map((article) => {
+    return (
+      <div key={article.title}>
+        <div className="newsletter">
+          <a href={article.link} className="newsletter-content">
+            <Image
+              src={article.image}
+              width={600}
+              height={400}
+              alt="More than 200 books in Amazon's bookstore have ChatGPT listed as an author or coauthor"
+            />
+            <h4 className="title">{article.title}</h4>
+
+            <p className="attribution">{article.attribution}</p>
+            <p className="content-body">{article.body}</p>
+          </a>
+        </div>
+      </div>
+    )
+  })
+
+  return (
+    <Layout title="Document" description="Document description">
+      <div className="home-heading">
+        <h1>How AI is impacting tech, the arts and mankind</h1>
+        <h2>Westland Post</h2>
+      </div>
+      <main className="two-column">
+        {/* <iframe src="https://www.westland.net" width="100%"></iframe> */}
+        {/* <iframe
+          width="540"
+          height="305"
+          src="https://26466fad.sibforms.com/serve/MUIEAK5CWenSsLC09gmZhi5Z-3TqvrSW1sUd7vEbSInlbipBoSHSK0B-MQoyJUtv8dYjeZD9U8BopaZFWNS_RC7htR_Pot3YikfPXMMfZhBThYQ1PLEUtVslhcEJZqHDPS9BIpnwAsIH7C3L42YPWiiG1IJN8dP6BgwvoV4qXh_VLfpP8b9efKLYQbDUSDSGY9dswcAKlhDCp7TQ"
+
+          scrolling="auto"
+          allowfullscreen
+          // style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"
+        ></iframe> */}
+        <section>
+          <h3>Previous Posts</h3>
+          {articles}
+        </section>
+
+        <aside>
+          <h3>Image Gallery</h3>
+          <Image
+            src="/images/grace-hopper.jpg"
+            width={600}
+            height={400}
+            alt="Grace Hopper"
+          />
+          <br />
+          <br />
+          <Image src="/images/eniac.jpg" width={600} height={400} alt="ENIAC" />
+          <br />
+          <br />
+          <Image src="/images/ibm.jpg" width={600} height={400} alt="IBM" />
+        </aside>
+      </main>
+    </Layout>
+  )
+}
 export default TwoColumnPage
---
+++ src/common/content-data.ts
@@ -0,0 +1,49 @@
+const contentData = [
+  {
+    image: '/content-images/chatgpt-on-laptop.jpg',
+    title:
+      "More than 200 books in Amazon's bookstore have ChatGPT listed as an author or coauthor",
+    attribution: 'Business Insider - Beatrice Nolan',
+    body: "ChatGPT, has been credited as the author or coauthor of over 200 books on Amazon's bookstore, raising concerns about the potential for a flood of low-quality books. Some of the books are guides to using the chatbot, while others are children's books written by ChatGPT and illustrated by other AI programs. Amazon policies do not require users to disclose the use of AI, so the real number of books authored by ChatGPT could be higher. The trend has been criticized by some, who fear that it could undermine the value of creative writing and put many authors out of work.",
+    link: 'https://www.businessinsider.com/chatgpt-ai-write-author-200-books-amazon-2023-2',
+  },
+  {
+    image: '/content-images/special-ops.jpg',
+    title:
+      'Yikes, the U.S. Is Now Using Facial Recognition Rigged Drones for Special Ops',
+    attribution: 'Gizmodo - Lucas Ropek',
+    body: 'The US government has been using drones equipped with facial recognition technology to aid in special operations missions, according to New Scientist magazine. The drones are sold by a Seattle-based firm called RealNetworks, which provides a platform called Secure Accurate Facial Recognition, or SAFR. While the drones are not typically armed, they could be used for intelligence and target acquisition. Critics have warned of the ethical implications of using the technology.',
+    link: 'https://gizmodo.com/drones-facial-recognition-us-air-force-realnetworks-1850163798',
+  },
+  {
+    image: '/content-images/robot-in-kitchen.jpg',
+    title: 'Is ChatGPT Coming for Your Kitchen?',
+    attribution: 'Eater -  Megan Ulu-Lani Boyanton',
+    body: "TikTok creator Michelle Meng, is testing AI-generated recipes against recipes from professional chefs and restaurants on her channel. So far, there has been no clear pattern in the success of either method, although AI seems to do better with more complex ingredients. While some viewers have raised concerns about the ethics of using other creators' recipes in this way, Meng argues that AI is more of a tool than a replacement for human chefs. Although there has been interest in the videos, it remains to be seen whether AI-generated recipes will gain widespread popularity.",
+    link: 'https://www.eater.com/23620766/chatgpt-ai-recipes-versus-chefs-tiktok-who-made-it-better',
+  },
+  {
+    image: '/content-images/chatgpt-api.jpg',
+    title: 'Introducing ChatGPT and Whisper APIs',
+    attribution: 'OpenAI',
+    body: "OpenAI releases cost-effective ChatGPT and Whisper models on their API for language and speech-to-text capabilities, with significant cost savings and model improvements. ChatGPT API has been adopted by various companies, including Snapchat, Quizlet, Instacart, and Shopify, for their innovative AI-powered products. The new ChatGPT model family, gpt-3.5-turbo, is priced at $0.002 per 1k tokens, making it 10x cheaper than their existing GPT-3.5 models, and can be used for non-chat use cases. The API's new endpoint interacts with ChatGPT models, which consume messages and metadata using a new format called Chat Markup Language.",
+    link: 'https://openai.com/blog/introducing-chatgpt-and-whisper-apis',
+  },
+  {
+    image: '/content-images/fake-news-frenzy.jpg',
+    title:
+      'A fake news frenzy: why ChatGPT could be disastrous for truth in journalism',
+    attribution: 'The Guardian - Emily Bell',
+    body: 'ChatGPT has the potential to generate vast amounts of news content. However, the platform has "no commitment to the truth" and has prompted fears it could be used to create fake news stories that appear to have been written by humans, warns journalist Emily Bell. In addition, Bell highlights the ethical issues with OpenAI itself, after it emerged workers in Kenya had been paid less than $2 an hour to sift through graphic content. The hype surrounding ChatGPT has drowned out warnings from experts about the potential risks of the platform, says Bell.',
+    link: 'https://www.theguardian.com/commentisfree/2023/mar/03/fake-news-chatgpt-truth-journalism-disinformation',
+  },
+  {
+    image: '/content-images/ai-video-tools.jpg',
+    title: 'VIDEO: AI Video & Audio Editing Tools (Big Time Save!)',
+    attribution: 'YouTube - Premiere Gal ',
+    body: "Premiere Gal presents a video about AI Video and Audio Editing Tools, featuring Adobe Premiere Pro's Sensei AI Tools and Effects, EndBoost, RunwayML, and Deep Swap. The tools include auto-reframe, color, remix, AI powered transcription, auto-ducking, sound mixing, motion tracking, face blur, and deep fake face swap.",
+    link: 'https://www.youtube.com/watch?v=rLJQFI8GdxY',
+  },
+]
+
+export default contentData
