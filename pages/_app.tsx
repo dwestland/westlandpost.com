70e8f2dd38f16d9c3b1df7d3792e13767318255a
dwestland <don@westland.net>
2023-04-01 12:40:42 -0700

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { AppProps } from 'next/app'
 import React from 'react'
 import '@/styles/main.scss'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import Script from 'next/script'
 
 const queryClient = new QueryClient()
 
@@ -13,11 +14,26 @@ export default function App({
   pageProps: { ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={(pageProps as any).session} refetchInterval={0}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </SessionProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KX9Q0KE2ZE"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-KX9Q0KE2ZE');
+        `}
+      </Script>
+      <SessionProvider session={(pageProps as any).session} refetchInterval={0}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </SessionProvider>
+    </>
   )
 }
